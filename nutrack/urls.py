bd8e5d9aa6dd69bbc55f90189039c8372a95af9a
Muhammad Akmal Hakim <akmalhakim2106@gmail.com>
2022-10-26 02:51:16 +0700

    akmaalh/fixing

--- nutrack/urls.py
+++ nutrack/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from main.views import login_user
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path(r'login/', login_user),
 ]
+
+urlpatterns += staticfiles_urlpatterns()

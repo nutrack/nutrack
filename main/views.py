1ddb04ee9223b22b6554184d655c41a45ae0669b
sashanabilafortuna <sashanabilafortuna@gmail.com>
2022-10-27 18:19:54 +0700

    sasha/fix login

--- main/views.py
+++ main/views.py
@@ -36,8 +36,9 @@ def register(request):
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password1')
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username, email=email, password=password)
         print(user)
         if user is not None:
             login(request, user) # melakukan login terlebih dahulu

cd5a8ac623a0ae0c3f9d69e0227cd8e216c20d21
sashanabilafortuna <sashanabilafortuna@gmail.com>
2022-10-30 15:08:28 +0700

    sasha/edit model

--- calorycalc/models.py
+++ calorycalc/models.py
@@ -1,6 +1,7 @@
-from email.policy import default
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 class caloryInfo(models.Model):

fb208ec28a68fd80c1968680d1ebbdf103cbb82d
sherlockjack <sherlocknagata@gmail.com>
2022-10-31 20:30:14 +0700

    Fix forms.py testimonies

--- testimonies/urls.py
+++ testimonies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import show_testimonies
+from testimonies.views import show_testimonies
 
 app_name = 'testimonies'
-
+ 
 urlpatterns = [
     path('', show_testimonies, name='show_testimonies'),
 ]

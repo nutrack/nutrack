a00a770a4051af0a917d2315bb151135608f6571
Muhammad Akmal Hakim <akmalhakim2106@gmail.com>
2022-10-31 18:42:06 +0700

    akmaalh

--- food_rec/models.py
+++ food_rec/models.py
@@ -1,15 +1,13 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
 class Food(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    calories = models.IntegerField()
-    protein = models.IntegerField()
-    fat = models.IntegerField()
-    carbs = models.IntegerField()
-    is_food = models.BooleanField(default=True)
-    rating = models.IntegerField(default=0);
-    def __str__(self):
-        return self.name
+    name = models.TextField(max_length=100, default='', blank=True, null=True)
+    calories = models.IntegerField(default=0, blank=True, null=True)
+    protein = models.IntegerField(default=0, blank=True, null=True)
+    fat = models.IntegerField(default=0, blank=True, null=True)
+    carbs = models.IntegerField(default=0, blank=True, null=True)
+    is_food = models.BooleanField(default=True , blank=True, null=True)
+    rating = models.IntegerField(default=0 , blank=True, null=True)

d456971a4cee88830019df703f4d015047c76b9f
farkhans <farkhansyawalharahap@gmail.com>
2022-10-31 21:13:41 +0700

    testimonies stuff

--- testimonies/urls.py
+++ testimonies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from testimonies.views import show_testimonies
+from testimonies.views import *
 
 app_name = 'testimonies'
  
 urlpatterns = [
     path('', show_testimonies, name='show_testimonies'),
+    path('create-testimonies', create_testimonies, name='create_testimonies'),
 ]

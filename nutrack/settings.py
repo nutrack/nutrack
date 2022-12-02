3f671e2ccc2725d2f4072933347659366c4ed292
sashanabilafortuna <sashanabilafortuna@gmail.com>
2022-12-02 08:29:08 +0700

    cant login again T__T

--- nutrack/settings.py
+++ nutrack/settings.py
@@ -72,6 +72,12 @@ CORS_ORIGIN_WHITELIST = [
     'https://nu-track.up.railway.app/'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'https://nu-track.up.railway.app/'
+]
+
 ROOT_URLCONF = 'nutrack.urls'
 
 TEMPLATES = [

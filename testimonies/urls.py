aff193bbae115a1e716aa25c6ac6a1c47c03c409
farkhans <farkhansyawalharahap@gmail.com>
2022-12-10 11:29:25 +0700

    add url

--- testimonies/urls.py
+++ testimonies/urls.py
@@ -6,6 +6,7 @@ app_name = 'testimonies'
 urlpatterns = [
     path('', show_testimonies, name='show_testimonies'),
     path('create-testimonies', create_testimonies, name='create_testimonies'),
+    path('create-testimonies-flutter', create_testimonies_flutter, name='create_testimonies_flutter'),
     path('json', testimonies_json, name='testimonies_json'),
     path('add/', create_testimony_ajax, name='create_testimony_ajax')
 ]

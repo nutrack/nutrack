896f9ca7e52f689359d41433270c288e0c7f7389
Muhammad Akmal Hakim <akmalhakim2106@gmail.com>
2022-12-12 13:46:17 +0700

    fix views

--- food_rec/models.py
+++ food_rec/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Food(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.TextField(max_length=100, default='', blank=True, null=True)
     calories = models.IntegerField(default=0, blank=True, null=True)
     protein = models.IntegerField(default=0, blank=True, null=True)

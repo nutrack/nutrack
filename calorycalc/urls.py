f3939a1d393a50ee4e91c4395ef351ace6c5f96d
sherlockjack <sherlocknagata@gmail.com>
2022-12-12 15:14:50 +0700

    semoga

--- calorycalc/urls.py
+++ calorycalc/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
-from calorycalc.views import add_calory, calorycalc, histories, history, show_calory_json, show_history_by_id_json, test, add_calory_flutter
+from calorycalc.views import add_calory, calorycalc, histories, history, show_calory_json, show_history_by_id_json, test, add_calory_flutter.show_calory_json_flutter
 app_name = 'calorycalc'
 
 urlpatterns = [
     path('', calorycalc, name='calorycalc'),
     path("json/", show_calory_json, name="show_calory_json"),
+    path("json/flutter", show_calory_json_flutter, name="show_calory_json_flutter"),
     path("add_calory/", add_calory, name="add_calory"),
     path('add_calory_flutter/', add_calory_flutter, name='add_calory_flutter'),
     path("history/<int:id>",history,name="history"),
